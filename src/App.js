6f48355f459eb21b5672dd6a699c05ca70759890
Joshua dela Cruz <joshua@ecommercewebsites.com.au>
2019-12-04 22:20:57 +0800

    created collection and collection preview components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path='/' exact component={HomePage}></Route>
+        <Route path='/shop' exact component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
