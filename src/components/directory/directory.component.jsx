970ccdff47e2d4b38c0071cb6baf85626dd19012
Joshua dela Cruz <joshua@ecommercewebsites.com.au>
2019-12-04 20:51:58 +0800

    adds navigation using react dom

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,14 +50,8 @@ class Directory extends Component {
     return (
       <div className="directory-menu">
       {
-        this.state.sections.map(({ id, title, imageUrl, size }) => 
-          <MenuItem 
-            key={id}
-            title={title}
-            subtitle='shopnow'
-            image={imageUrl}
-            size={size}
-          />
+        this.state.sections.map(({ id, ...otherProps }) => 
+          <MenuItem key={id} {...otherProps}/>
         )
       }
       </div>
